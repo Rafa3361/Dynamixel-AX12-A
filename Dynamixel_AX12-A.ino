2926893db43245e292a3327c568ba745e081b58d
_RC_Santos <117594326+Rafa3361@users.noreply.github.com>
2024-05-22 18:33:56 -0300

    Create Dynamixel_AX12-A.ino

---
+++ Dynamixel_AX12-A.ino
@@ -0,0 +1,23 @@
+#include <AX12A.h> // Verificar o download desta biblioteca
+
+#define DirectionPin  (1u) // Porta digital sempre 1 (TX)
+#define BaudRate    (1000000ul)
+#define ID    (6u) // ID do seu motor
+
+
+void setup()
+{
+ ax12a.begin(BaudRate, DirectionPin, &Serial);
+ ax12a.setEndless(ID, ON);
+ 
+}
+
+void loop()
+{ 
+    ax12a.ledStatus(ID, ON); // Controle do led 
+    ax12a.turn(ID, RIGHT, 1000); // Valor de 0 à 1023 (1024 bits)
+}
+
+
+// Para ter acesso a todos os comandos da biblioteca:
+// https://github.com/jumejume1/AX-12A-servo-library/blob/master/keywords.txt
